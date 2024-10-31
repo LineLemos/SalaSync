a9d8ef50ea042a3d1cba2c5f4d0a5591d968238e
Aline Lemos <alinelemos@id.uff.br>
2024-10-31 18:33:24 -0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import "./css/App.css"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="header">
+        <a href="#">Home</a>
+        <p>ola</p>
+        <a href="#">Salas</a>
+        <a href="#">Senai</a>
+      </div>
+    </>
+  );
+}
+
+export default App
